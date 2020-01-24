97bb969766d0c0694a976bf306783abd9949aeee
MelanX <meissi97@me.com>
2020-01-24 17:17:05 +0100

    removed config
    you will not be able to turn off the extra hammers muhaha

--- src/main/java/de/melanx/vanillaexcavators/VanillaExcavators.java
+++ src/main/java/de/melanx/vanillaexcavators/VanillaExcavators.java
@@ -1,12 +1,8 @@
 package de.melanx.vanillaexcavators;
 
 import de.melanx.vanillaexcavators.common.ExcavatorRegistry;
-import de.melanx.vanillaexcavators.config.VanillaExcavatorsConfig;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.loading.FMLPaths;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -18,8 +14,6 @@ public class VanillaExcavators {
 
     public VanillaExcavators() {
         instance = this;
-        ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, VanillaExcavatorsConfig.SERVER_CONFIG);
-        VanillaExcavatorsConfig.loadConfig(VanillaExcavatorsConfig.SERVER_CONFIG, FMLPaths.CONFIGDIR.get().resolve(MODID + "-server.toml"));
         ExcavatorRegistry.registerHammers();
         MinecraftForge.EVENT_BUS.register(this);
     }
