c4b093f0abc219abf99893d5834725a98c022148
MelanX <meissi97@me.com>
2020-01-24 15:44:06 +0100

    renamed interface

--- src/main/java/de/melanx/vanillaexcavators/api/IBreakValidator.java
+++ src/main/java/de/melanx/vanillaexcavators/api/IBreakValidator.java
@@ -2,7 +2,7 @@ package de.melanx.vanillaexcavators.api;
 
 import net.minecraft.block.BlockState;
 
-public interface BreakValidator {
+public interface IBreakValidator {
 
     boolean canBreak(BlockState state);
 }
