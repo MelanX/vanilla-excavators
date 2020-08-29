aa9bdd6bfe809f983079868478eabab072084163
MelanX <info@melanx.de>
2020-08-29 20:56:11 +0200

    fixed able to break pickaxe blocks

--- src/main/java/de/melanx/vanillaexcavators/items/ExcavatorItem.java
+++ src/main/java/de/melanx/vanillaexcavators/items/ExcavatorItem.java
@@ -1,5 +1,6 @@
 package de.melanx.vanillaexcavators.items;
 
+import com.google.common.collect.ImmutableSet;
 import de.melanx.morevanillalib.api.BigBreakItem;
 import net.minecraft.block.BlockState;
 import net.minecraft.item.IItemTier;
@@ -8,7 +9,7 @@ import net.minecraftforge.common.ToolType;
 public class ExcavatorItem extends BigBreakItem {
 
     public ExcavatorItem(IItemTier toolMaterial, float attackSpeed) {
-        super(toolMaterial, attackSpeed);
+        super(toolMaterial, attackSpeed, ImmutableSet.of(), ToolType.SHOVEL);
     }
 
     @Override
