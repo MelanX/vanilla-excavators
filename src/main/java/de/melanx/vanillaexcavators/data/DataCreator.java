82109dc87356f924f903f143bb33c318407d1af8
MelanX <info@melanx.de>
2020-11-11 18:21:56 +0100

    update dependencies

--- src/main/java/de/melanx/vanillaexcavators/data/DataCreator.java
+++ src/main/java/de/melanx/vanillaexcavators/data/DataCreator.java
@@ -3,7 +3,7 @@ package de.melanx.vanillaexcavators.data;
 import de.melanx.vanillaexcavators.VanillaExcavators;
 import net.minecraft.data.BlockTagsProvider;
 import net.minecraft.data.DataGenerator;
-import net.minecraftforge.client.model.generators.ExistingFileHelper;
+import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.GatherDataEvent;
--- src/main/java/de/melanx/vanillaexcavators/data/ItemModels.java
+++ src/main/java/de/melanx/vanillaexcavators/data/ItemModels.java
@@ -4,8 +4,10 @@ import de.melanx.vanillaexcavators.VanillaExcavators;
 import de.melanx.vanillaexcavators.items.ExcavatorRegistry;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.item.Item;
-import net.minecraftforge.client.model.generators.ExistingFileHelper;
+import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.fml.RegistryObject;
+
+import javax.annotation.Nonnull;
 
 public class ItemModels extends de.melanx.morevanillalib.data.ItemModels {
     public ItemModels(DataGenerator generator, ExistingFileHelper helper) {
@@ -18,6 +20,7 @@ public class ItemModels extends de.melanx.morevanillalib.data.ItemModels {
             this.generateItem(item.get());
     }
 
+    @Nonnull
     @Override
     public String getName() {
         return VanillaExcavators.MODID + " item models";
