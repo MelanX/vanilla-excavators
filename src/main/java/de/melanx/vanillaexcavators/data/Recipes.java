6595f88e7a0f667918af7d80c25899ac2d922006
MelanX <info@melanx.de>
2020-11-13 15:53:11 +0100

    change recipes for new condition
    fixes #4

--- src/main/java/de/melanx/vanillaexcavators/data/Recipes.java
+++ src/main/java/de/melanx/vanillaexcavators/data/Recipes.java
@@ -1,6 +1,9 @@
 package de.melanx.vanillaexcavators.data;
 
+import com.google.gson.JsonArray;
 import de.melanx.morevanillalib.api.BigBreakMaterials;
+import de.melanx.morevanillalib.core.WrapperResult;
+import de.melanx.morevanillalib.core.crafting.VanillaCondition;
 import de.melanx.vanillaexcavators.items.ExcavatorRegistry;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.IFinishedRecipe;
@@ -11,6 +14,7 @@ import net.minecraft.item.Items;
 import net.minecraft.tags.ITag;
 import net.minecraftforge.fml.RegistryObject;
 
+import javax.annotation.Nonnull;
 import java.util.function.Consumer;
 
 public class Recipes extends RecipeProvider {
@@ -20,13 +24,21 @@ public class Recipes extends RecipeProvider {
     }
 
     @Override
-    protected void registerRecipes(Consumer<IFinishedRecipe> consumer) {
+    protected void registerRecipes(@Nonnull Consumer<IFinishedRecipe> consumer) {
         int i = 0;
         for (RegistryObject<Item> item : ExcavatorRegistry.ITEMS.getEntries()) {
             BigBreakMaterials excavator = BigBreakMaterials.values()[i];
             ITag.INamedTag<Item> tagIngredient = excavator.getTagIngredient();
             ShapedRecipeBuilder recipe = createRecipe(item.get(), tagIngredient);
-            recipe.build(consumer);
+            if (!excavator.isVanilla()) {
+                recipe.build(WrapperResult.transformJson(consumer, json -> {
+                    JsonArray array = new JsonArray();
+                    array.add(VanillaCondition.SERIALIZER.getJson(new VanillaCondition(true)));
+                    json.add("conditions", array);
+                }));
+            } else {
+                recipe.build(consumer);
+            }
             i++;
         }
     }
